f875ddf232469e62295c30afc5741b11c05e5d86
Christopher Dorst <chris@dorst.email>
2017-12-28 15:58:36 -0600

    Add GitCheckoutCommandRunner

---
+++ DevOps.VersionControl.Functions.RunGitCheckoutCommand/GitCheckoutCommandRunner.cs
@@ -0,0 +1,14 @@
+﻿using static DevOps.VersionControl.Functions.RunGitCommand.GitCommandRunner;
+
+namespace DevOps.VersionControl.Functions.RunGitCheckoutCommand
+{
+    public static class GitCheckoutCommandRunner
+    {
+        private const string checkout = nameof(checkout);
+
+        public static void Checkout(string directory, params string[] arguments)
+            => Git(directory,
+                command: checkout,
+                arguments);
+    }
+}
